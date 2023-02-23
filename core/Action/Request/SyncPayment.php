c9e1ea1549170a25861da9fe9242012c1d47582e
Lukasz Falda <lukasz.falda@wearco.pl>
2023-02-23 12:18:27 +0100

    Promoted property for SyncPayment

--- core/Action/Request/SyncPayment.php
+++ core/Action/Request/SyncPayment.php
@@ -6,28 +6,15 @@ namespace Answear\Payum\Action\Request;
 
 use Payum\Core\Model\ModelAggregateInterface;
 use Payum\Core\Model\PaymentInterface;
-use Webmozart\Assert\Assert;
 
 class SyncPayment implements ModelAggregateInterface
 {
-    private PaymentInterface $model;
-
-    public function __construct(PaymentInterface $model)
+    public function __construct(private readonly PaymentInterface $model)
     {
-        $this->setModel($model);
     }
 
     public function getModel(): PaymentInterface
     {
         return $this->model;
-    }
-
-    /**
-     * @param PaymentInterface $model
-     */
-    public function setModel($model): void
-    {
-        Assert::isInstanceOf($model, PaymentInterface::class);
-        $this->model = $model;
     }
 }
