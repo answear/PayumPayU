8be5269bba4fec6177ffcd6967b64da18f937d03
Lukasz Falda <lukasz.falda@wearco.pl>
2023-05-25 15:13:00 +0200

    Single refund not found test

--- src/Enum/ResponseStatusCode.php
+++ src/Enum/ResponseStatusCode.php
@@ -8,10 +8,16 @@ enum ResponseStatusCode: string
 {
     case Success = 'SUCCESS';
     case ErrorValueMissing = 'ERROR_VALUE_MISSING';
-    case BusinessError = 'BUSINESS_ERROR';
     case ErrorValueInvalid = 'ERROR_VALUE_INVALID';
-    case ErrorInternal = 'ERROR_INTERNAL';
     case OpenpayuBusinessError = 'OPENPAYU_BUSINESS_ERROR';
     case OpenpayuErrorValueInvalid = 'OPENPAYU_ERROR_VALUE_INVALID';
     case OpenpayuErrorInternal = 'OPENPAYU_ERROR_INTERNAL';
+    case Unauthorized = 'UNAUTHORIZED';
+    case DataNotFound = 'DATA_NOT_FOUND';
+    case Timeout = 'TIMEOUT';
+    case BusinessError = 'BUSINESS_ERROR';
+    case ErrorInternal = 'ERROR_INTERNAL';
+    case GeneralError = 'GENERAL_ERROR';
+    case Warning = 'WARNING';
+    case ServiceNotAvailable = 'SERVICE_NOT_AVAILABLE';
 }
--- src/Exception/PayUNetworkException.php
+++ src/Exception/PayUNetworkException.php
@@ -4,6 +4,23 @@ declare(strict_types=1);
 
 namespace Answear\Payum\PayU\Exception;
 
+use GuzzleHttp\Exception\ClientException;
+use Psr\Http\Message\ResponseInterface;
+
 class PayUNetworkException extends PayUException
 {
+    public function __construct(string $message = '', int $code = 0, ?\Throwable $previous = null)
+    {
+        $this->setOriginalResponse($previous);
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    private function setOriginalResponse(?\Throwable $previous): void
+    {
+        if ($previous instanceof ClientException && $previous->getResponse() instanceof ResponseInterface) {
+            $originalResponse = $previous->getResponse()->getBody()->getContents();
+            $this->response = json_decode($originalResponse, true, 512, JSON_THROW_ON_ERROR);
+        }
+    }
 }
